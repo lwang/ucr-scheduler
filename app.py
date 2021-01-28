49c30ce04a47cd35a47e90f3dc448c881d7e3343
Lawrence Wang <lawrence.g.wang@gmail.com>
2021-01-28 00:11:27 -0800

    Experiments with Flask and Heroku to figure out how this API will work with Github pages

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -5,4 +6,15 @@ app = Flask(__name__)
 def home():
     term = request.args['term']
     course = request.args['course']
-    return {'hello':term, 'world':course}
+
+    s = requests.Session()
+    s.get(f'https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/term/search?mode=search&dataType=json'
+            f'&term={term}&studyPath=&studyPathText=&startDatepicker=&endDatepicker=')
+    jsonurl = f'https://registrationssb.ucr.edu/StudentRegistrationSsb/ssb/searchResults/searchResults?' \
+                f'txt_subjectcoursecombo={course}&txt_term={term}&startDatepicker=&endDatepicker=' \
+                f'&pageOffset=0&pageMaxSize=999&sortColumn=subjectDescription&sortDirection=asc&[object%20Object]'
+    parsed_json = s.get(jsonurl).json()
+    class_data = [class_dict for class_dict in parsed_json['data']]
+    return class_data
+
+    # return {'hello':term, 'world':course}
