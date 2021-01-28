e627c7ba2703f58a4368d3a3269310fbfee43f54
Lawrence Wang <lawrence.g.wang@gmail.com>
2021-01-28 00:43:06 -0800

    Added flask_cors package to fix CORS error when sending a request from Github page and added gitignore

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import requests
 from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def home():
