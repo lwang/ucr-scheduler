6dc16c71c249da99a74b21efb90fc01b433cd4ea
Lawrence Wang <lawrence.g.wang@gmail.com>
2021-02-05 21:15:22 -0800

    .

---
+++ store.js
@@ -0,0 +1,21 @@
+import { writable } from "svelte/store";
+
+export const theme = localStorage.getItem('theme') ? writable(JSON.parse(localStorage.getItem('theme'))) : writable('dark');
+theme.subscribe(value => {
+    localStorage.setItem("theme", JSON.stringify(value));
+});
+
+export const active = localStorage.getItem('active') ? writable(JSON.parse(localStorage.getItem('active'))) : writable('Select Term');
+active.subscribe(value => {
+    localStorage.setItem("active", JSON.stringify(value));
+});
+
+export const term = localStorage.getItem('term') ? writable(JSON.parse(localStorage.getItem('term'))) : writable('');
+term.subscribe(value => {
+    localStorage.setItem("term", JSON.stringify(value));
+});
+
+export const courses = localStorage.getItem('courses') ? writable(JSON.parse(localStorage.getItem('courses'))) : writable([]);
+courses.subscribe(value => {
+    localStorage.setItem("courses", JSON.stringify(value));
+});
